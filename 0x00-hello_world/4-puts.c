ce091d0aed97da8259f366c5ce96172f086c9f5b
kingvictor00 <victorgodwin68@gmail.com>
2022-07-03 16:59:23 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *main - code to put a message
+ *Return: Always 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
